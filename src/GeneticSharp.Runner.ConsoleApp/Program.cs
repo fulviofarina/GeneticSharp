81b266ba71ba45cd5480308221622b0adaa87d29
fulviofarina <fulviofarina@gmail.com>
2017-02-02 19:41:31 -0400

    v0.1

--- src/GeneticSharp.Runner.ConsoleApp/Program.cs
+++ src/GeneticSharp.Runner.ConsoleApp/Program.cs
@@ -5,26 +5,73 @@ using GeneticSharp.Domain.Populations;
 using GeneticSharp.Infrastructure.Framework.Reflection;
 using GeneticSharp.Infrastructure.Threading;
 using GeneticSharp.Runner.ConsoleApp.Samples;
+using GeneticSharp.Domain.Chromosomes;
+
+using System.Collections.Generic;
 
 namespace GeneticSharp.Runner.ConsoleApp
 {
-    public class Program
+    public static class Msg
     {
-        public static void Main(string[] args)
+
+        ////////////////////////////////
+        public static void EvolvedMsg()
         {
-            Run();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine();
+            Console.WriteLine("Evolved.");
+            Console.ResetColor();
         }
 
-        private static void Run()
+        public static void ErrorMsg(ref Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine();
+            Console.WriteLine("Error: {0}", ex.Message);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+
+
+
+
+
+        ////////////////////////MINE
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void IntroMsg()
         {
             Console.SetError(TextWriter.Null);
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine("GeneticSharp - ConsoleApp");
             Console.ResetColor();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="selectedSampleName"></param>
+
+        public static void DrawSampleName(string selectedSampleName)
+        {
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("GeneticSharp - ConsoleApp");
+            Console.WriteLine();
+            Console.WriteLine(selectedSampleName);
+            Console.ResetColor();
+        }
+
+        public static string SelectSample(ref IList<string> sampleNames)
+        {
             Console.WriteLine("Select the sample:");
 
-            var sampleNames = TypeHelper.GetDisplayNamesByInterface<ISampleController>();
 
             for (int i = 0; i < sampleNames.Count; i++)
             {
@@ -44,35 +91,38 @@ namespace GeneticSharp.Runner.ConsoleApp
                 Console.WriteLine("Invalid option.");
             }
 
-            var sampleController = TypeHelper.CreateInstanceByName<ISampleController>(selectedSampleName);
-            DrawSampleName(selectedSampleName);
-            sampleController.Initialize();
+            return selectedSampleName;
+        }
 
-            Console.WriteLine("Starting...");
+    }
+}
 
-            var selection = sampleController.CreateSelection();
-            var crossover = sampleController.CreateCrossover();
-            var mutation = sampleController.CreateMutation();
-            var fitness = sampleController.CreateFitness();
-            var population = new Population(100, 200, sampleController.CreateChromosome());
-            population.GenerationStrategy = new PerformanceGenerationStrategy();
+namespace GeneticSharp.Runner.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //intro text
+            Msg.IntroMsg();
 
-            var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
-            ga.Termination = sampleController.CreateTermination();            
 
-            var terminationName = ga.Termination.GetType().Name;
+            //get list of examples (menu)
+            IList<string> sampleNames = TypeHelper.GetDisplayNamesByInterface<ISampleController>();
 
-            ga.GenerationRan += delegate
-            {
-                DrawSampleName(selectedSampleName);
+            //select an example
+            string selectedSampleName = Msg.SelectSample(ref sampleNames);
 
-                var bestChromosome = ga.Population.BestChromosome;
-                Console.WriteLine("Termination: {0}", terminationName);
-                Console.WriteLine("Generations: {0}", ga.Population.GenerationsNumber);
-                Console.WriteLine("Fitness: {0,10}", bestChromosome.Fitness);
-                Console.WriteLine("Time: {0}", ga.TimeEvolving);
-                sampleController.Draw(bestChromosome);
-            };
+        
+            
+            //an interface for the example to use?
+            ISampleController sampleController = null;
+            GeneticAlgorithm ga = null; //object engine
+
+
+            ga = Create(selectedSampleName, ref sampleController); //creates the thing
+
+            ConfigHandler(selectedSampleName, ref sampleController, ref ga);
 
             try
             {
@@ -81,31 +131,83 @@ namespace GeneticSharp.Runner.ConsoleApp
             }
             catch (Exception ex)
             {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine();
-                Console.WriteLine("Error: {0}", ex.Message);
-                Console.ResetColor();
-                Console.ReadKey();
+                Msg.ErrorMsg(ref ex);
                 return;
             }
 
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine();
-            Console.WriteLine("Evolved.");
-            Console.ResetColor();
+            Msg.EvolvedMsg();
+
             Console.ReadKey();
-            Run();
+
+            Main(args);
+
         }
 
-        private static void DrawSampleName(string selectedSampleName)
+   
+
+        private static GeneticAlgorithm Create(string selectedSampleName, ref ISampleController sampleController)
         {
-            Console.Clear();
+            //get sample controller associated to sample name
+            sampleController = TypeHelper.CreateInstanceByName<ISampleController>(selectedSampleName);
 
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("GeneticSharp - ConsoleApp");
-            Console.WriteLine();
-            Console.WriteLine(selectedSampleName);
-            Console.ResetColor();
+            Msg.DrawSampleName(selectedSampleName); //text
+
+            sampleController.Initialize(); //IMPORTANT
+
+            Console.WriteLine("Starting...");
+
+           Domain.Selections.ISelection selection = sampleController.CreateSelection();
+           Domain.Crossovers.ICrossover crossover = sampleController.CreateCrossover();
+            Domain.Mutations.IMutation mutation = sampleController.CreateMutation();
+            Domain.Fitnesses.IFitness fitness = sampleController.CreateFitness();
+            Domain.Populations.IPopulation  population = new Population(100, 200, sampleController.CreateChromosome());
+            population.GenerationStrategy = new PerformanceGenerationStrategy();
+
+            GeneticAlgorithm ga;
+            ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+            ga.Termination = sampleController.CreateTermination();
+
+
+
+            return ga;
+           
         }
+
+        private static void ConfigHandler(string selectedSampleName, ref ISampleController sampleController, ref GeneticAlgorithm geneticAlgo)
+        {
+
+            //REQUIRED CASTING to introduce them into the Lamda Expression
+            ISampleController scontroller = sampleController;
+            GeneticAlgorithm ga = geneticAlgo;
+
+
+            EventHandler generationRan;
+
+            generationRan = delegate
+            {
+                Msg.DrawSampleName(selectedSampleName);
+                string terminationName = ga.Termination.GetType().Name;
+
+                IChromosome bestChromosome = ga.Population.BestChromosome;
+                Console.WriteLine("Termination: {0}", terminationName);
+
+                int generationsNum = ga.Population.GenerationsNumber;
+                Console.WriteLine("Generations: {0}", generationsNum);
+
+                double? fitness = bestChromosome.Fitness;
+                Console.WriteLine("Fitness: {0,10}", fitness);
+
+                TimeSpan te = ga.TimeEvolving;
+                Console.WriteLine("Time: {0}", te);
+
+                scontroller.Draw(bestChromosome);
+            };
+
+            ga.GenerationRan += generationRan;
+            
+        }
+
+
+      
     }
 }
