d2dd918796977eee1912346fa214df076c68a735
fulviofarina <fulviofarina@gmail.com>
2017-02-11 18:08:38 -0400

    v1.7.2 added file to SQL Database Creation

---
+++ src/GADB/QueryToCreateSQLDataBase.sql
@@ -0,0 +1,228 @@
+USE [master]
+GO
+/****** Object:  Database [GA]    Script Date: 2/11/2017 18:06:55 ******/
+CREATE DATABASE [GA]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'GA', FILENAME = N'c:\GA.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'GA_log', FILENAME = N'c:\GA_log.ldf' , SIZE = 92864KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [GA].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [GA] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [GA] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [GA] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [GA] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [GA] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [GA] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [GA] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [GA] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [GA] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [GA] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [GA] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [GA] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [GA] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [GA] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [GA] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [GA] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [GA] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [GA] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [GA] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [GA] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [GA] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [GA] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [GA] SET RECOVERY FULL 
+GO
+ALTER DATABASE [GA] SET  MULTI_USER 
+GO
+ALTER DATABASE [GA] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [GA] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [GA] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [GA] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'GA', N'ON'
+GO
+USE [GA]
+GO
+/****** Object:  Table [dbo].[Conditions]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Conditions](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProblemID] [int] NULL,
+	[MaxA] [float] NULL,
+	[MaxB] [float] NULL,
+	[MaxC] [float] NULL,
+	[MinA] [float] NULL,
+	[MinB] [float] NULL,
+	[MinC] [float] NULL,
+	[AFine] [float] NULL,
+	[BFine] [float] NULL,
+	[CFine] [float] NULL,
+ CONSTRAINT [PK_KnapConditions] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Data]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Data](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProblemID] [int] NULL,
+	[A] [float] NULL,
+	[B] [float] NULL,
+	[C] [float] NULL,
+ CONSTRAINT [PK_KnapData] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[GA]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[GA](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Termination] [nvarchar](max) NULL,
+	[Crossover] [nvarchar](max) NULL,
+	[MutationProb] [float] NULL,
+	[CrossParents] [int] NULL,
+	[CrossChildren] [int] NULL,
+	[CrossChromeMinLenght] [int] NULL,
+	[CrossProbability] [float] NULL,
+	[Population] [nvarchar](max) NULL,
+	[PopMin] [int] NULL,
+	[PopMax] [int] NULL,
+	[GenerationTotal] [int] NULL,
+	[GenerationCurrent] [int] NULL,
+	[GenerationStrategy] [nvarchar](max) NULL,
+	[TimeStamp] [float] NULL,
+	[Fitness] [float] NULL,
+	[ProblemID] [int] NULL,
+	[ChromosomeLength] [int] NULL,
+ CONSTRAINT [PK_GA] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Problems]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Problems](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProblemID] [int] NULL,
+	[Label] [nvarchar](max) NULL,
+	[MinSize] [int] NULL,
+	[MaxSize] [int] NULL,
+	[Iters] [int] NULL,
+ CONSTRAINT [PK_Problems] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Solutions]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Solutions](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProblemID] [int] NULL,
+	[TotalWeight] [float] NULL,
+	[TotalVolume] [float] NULL,
+	[TotalValue] [float] NULL,
+	[WeightString] [nvarchar](max) NULL,
+	[VolumeString] [nvarchar](max) NULL,
+	[ValueString] [nvarchar](max) NULL,
+	[Genotype] [nvarchar](max) NULL,
+	[Fine] [float] NULL,
+	[Fitness] [float] NULL,
+	[DateTime] [datetime] NULL,
+	[TimeSpan] [float] NULL,
+	[Generations] [int] NULL,
+	[GAID] [int] NULL,
+	[Frequency] [int] NULL,
+	[SchOrder] [int] NULL,
+	[Okays] [nvarchar](max) NULL,
+	[Chromosome] [image] NULL,
+ CONSTRAINT [PK_KnapSolutions] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Strings]    Script Date: 2/11/2017 18:06:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Strings](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[ProblemID] [int] NULL,
+	[SolutionID] [int] NULL,
+	[TotalA] [float] NULL,
+	[TotalB] [float] NULL,
+	[TotalC] [float] NULL,
+	[AString] [nvarchar](max) NULL,
+	[BString] [nvarchar](max) NULL,
+	[CString] [nvarchar](max) NULL,
+	[GAID] [int] NULL,
+	[Okays] [nvarchar](max) NULL,
+	[Fine] [float] NULL,
+ CONSTRAINT [PK_KnapStrings] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+USE [master]
+GO
+ALTER DATABASE [GA] SET  READ_WRITE 
+GO
